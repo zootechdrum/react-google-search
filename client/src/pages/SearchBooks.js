3af448114f4865340b572b5aaa6b3b1b6d192a11
Cesar Gomez <zootechdrum@gmail.com>
2020-02-21 20:00:00 -0800

    can successfully render items for api call to thrid party API

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -3,10 +3,13 @@ import booksImg from "../images/books1.png"
 import API from "../utils/API";
 import Jumbotron from "../components/Jumbotron"
 import { Link } from "react-router-dom";
+import { List, ListItem } from "../components/List";
 
 
 class SearchBooks extends Component {
   state = {
+
+    booksData:["Hello"]
 
   };
 
@@ -16,18 +19,30 @@ class SearchBooks extends Component {
 
   loadBooks = () => {
     API.getBooks()
-    .then(res => console.log(res.data))
-  };
+    // .then(res => console.log(res.data))
+    .then(res => this.setState({ booksData: res.data.items }))
 
+
+  }
 
 
   render() {
     return (
         <div className="conatiner">
           <Jumbotron backgroundColor="#E6E6FA" >
-          <img className="img-fluid" src = {booksImg} alt="pokemon" />
+            <img className="img-fluid" src = {booksImg} alt="pokemon" />
           </Jumbotron>
-          
+          {this.state.booksData.length ? (
+              <List>
+                {this.state.booksData.map(book => (
+                  <ListItem >
+                    <p>{book.id}</p>
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
         </div>
     );
   }
