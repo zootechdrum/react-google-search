b25b4c342e0dae423d6aef50cfe5c796d1d52262
Cesar Gomez <zootechdrum@gmail.com>
2020-02-22 17:10:39 -0800

    added input

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -4,6 +4,7 @@ import API from "../utils/API";
 import Jumbotron from "../components/Jumbotron"
 import { Link } from "react-router-dom";
 import { List, ListItem } from "../components/List";
+import { Input, FormBtn } from "../components/Form";
 
 
 class SearchBooks extends Component {
@@ -46,19 +47,52 @@ class SearchBooks extends Component {
             <img className="img-fluid books-img" src = {booksImg} alt="pokemon" />
           </Jumbotron>
         <div className="container">
+          <div className="row">
+            <div class="col-12">
+              <form>
+                  <Input
+                    value={this.state.title}
+                    onChange={this.handleInputChange}
+                    name="title"
+                    placeholder="Title (required)"
+                  />
+                  <FormBtn
+
+                  >
+                    Submit Book
+                  </FormBtn>
+                </form>            
+            </div>
+          </div>
+          <div>
           {this.state.booksData.length ? (
+
               <List>
                 {this.state.booksData.map((book, index) => (
-                  <ListItem >
-                    <p>{book.volumeInfo.title}</p>
-                    <img className="img-fluid books-img" src = {book.volumeInfo.imageLinks.thumbnail} alt="pokemon" />
-                    <p>{book.searchInfo.textSnippet}</p>
-                  </ListItem>
+
+                  <div className="card p-3">
+                    <div className="row no-gutters">
+                      <div className="col-md-4 text-center">
+                      <img className="img-fluid books-img" src = {book.volumeInfo.imageLinks.thumbnail} alt="pokemon" />
+                      </div>
+                      <div className="col-md-8">
+                        <div className="card-body">
+                          <h5 className="card-title">Card title</h5>
+                          <p className="card-text">{book.searchInfo.textSnippet}</p>
+                          <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 ))}
+
               </List>
+
             ) : (
               <h3>No Results to Display</h3>
             )}
+
+        </div>
         </div>
         </div>
     );
