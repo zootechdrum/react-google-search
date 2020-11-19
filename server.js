22d44c77ee675a113820e712b40a6e549d1a1475
Cesar Gomez <zootechdrum@gmail.com>
2020-11-19 15:58:47 -0800

    updated env variables so the app can continue working

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
+require('dotenv').config()
+const express = require('express')
 
-require("dotenv").config();
-const express = require("express");
-
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const app = express();
-const PORT = process.env.PORT || 3001;
+const mongoose = require('mongoose')
+const routes = require('./routes')
+const app = express()
+const PORT = process.env.PORT || 3001
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
 }
 // Add routes, both API and view
-app.use(routes);
+app.use(routes)
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URIS || "mongodb://localhost/reactGoogleBooks");
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookSearch', {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+})
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+})
